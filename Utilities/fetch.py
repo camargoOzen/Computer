bbb5afb697efb72c2ad07a39d8ad985ddec3acc1
Juan Camargo <jucamargop@unal.edu.co>
2026-03-27 19:17:41 -0500

    refactor fetch.py

--- Utilities/fetch.py
+++ Utilities/fetch.py
@@ -1,14 +1,10 @@
 from RAM.dataRam import ram
 
-
 class Fetch:
     def __init__(self, ):
         self.data_ram = ram
         
-
     def fetch_instruction(self, address):
         instruction = ram.read(address)
         instruction_hex = hex(int(instruction, 16))[2:].upper()
-        return instruction_hex
-
-    
+        return instruction_hex    
