85f6e4c3b6a1394de5492b5f82e6d3006c5a60a6
Juan Camargo <jucamargop@unal.edu.co>
2026-03-27 19:18:32 -0500

    implement load_program2 function

--- Utilities/loader.py
+++ Utilities/loader.py
@@ -1,5 +1,6 @@
 from RAM.dataRam import ram
 import re
+
 class Loader:
     def __init__(self, data_ram, base_hex="0"):
         self.data_ram = data_ram
@@ -31,6 +32,23 @@ class Loader:
 
         print(f"Program loaded from '{file_path}'")
 
+    def load_program2(self, file: str):
+            # Expand (n) placeholders → 64-bit binary
+            def expand(match):
+                n = int(match.group(1))
+                return format(n & ((1 << 64) - 1), '064b')
+
+            for line in file.splitlines():
+                line = re.sub(r'\s+', '', line)
+                line = re.sub(r'\((\d+)\)', expand, line)
+
+                num_hex_chars = len(line) // 4
+                value = format(int(line, 2), f'0{num_hex_chars}X')
+
+                address = format(self.offset, '016X')
+                self.data_ram.write(address, value)
+                self.offset += 1
+
     def save_program(self, file_path, start_hex="0F", end_hex=None):
         start = int(start_hex, 16)
         if end_hex is not None:
@@ -52,5 +70,7 @@ class Loader:
                 file.write(binary + "\n")
         print(f"Program saved to '{file_path}'")
 
-loader = Loader(ram)
+    def set_base_hex(self, base_hex: str):
+        self.offset = int(base_hex, 16)
 
+loader = Loader(ram)
