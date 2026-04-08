7b0447b7578c55ec019b5ccf1156312e681e1d25
Juan Pablo Rodríguez Briceño <juanpisrodriguez@hotmail.com>
2026-04-07 23:27:59 -0500

    More changes for the preprocessor

--- Disco/Compilador/Preprocessor.py
+++ Disco/Compilador/Preprocessor.py
@@ -128,12 +128,6 @@ def preprocess_file(input_path, lib_dir=None, included_files=None):
                 define_value = tokens_list[2].value
                 macros[define_name] = define_value
 
-            elif tokens_list and tokens_list[0].type == 'OTHER' and len(tokens_list) > 2:
-                var_value = tokens_list[2].value
-                if tokens_list[2].type == 'OTHER' and var_value in macros:
-                    var_value = macros[tokens_list[2].value]
-                output_lines.append(f'{tokens_list[0].value} {tokens_list[1].value} {var_value}')
-
             elif tokens_list and tokens_list[0].type == 'OTHER' and len(tokens_list) > 3:
                 temp_instruction = tokens_list[0].value
                 temp_value1 = tokens_list[1].value
@@ -147,6 +141,14 @@ def preprocess_file(input_path, lib_dir=None, included_files=None):
                         temp_value2 = macros[temp_value2]
 
                 output_lines.append(f'{temp_instruction} {temp_value1} {temp_other} {temp_value2}')
+
+            elif tokens_list and tokens_list[0].type == 'OTHER' and len(tokens_list) > 2:
+                var_value = tokens_list[2].value
+                if tokens_list[2].type == 'OTHER' and var_value in macros:
+                    var_value = macros[tokens_list[2].value]
+                output_lines.append(f'{tokens_list[0].value} {tokens_list[1].value} {var_value}')
+
+            
             else:
                 output_lines.append(line)
 
